75fa4c3a14d062fc3f0d87ab4743e157d7230533
Seoin <tjdls111@naver.com>
2022-03-25 15:54:29 +0900

    chore : 하드코딩으로 데이터 수집

--- back/prap/views.py
+++ back/prap/views.py
@@ -34,6 +34,20 @@ def how_about_this(request):
 @api_view(['GET'])
 def popular(request):
     return Response({"lists":[
+        {"img":"https://dbm9jhyrx0h6k.cloudfront.net/service/1647308381931-a30dac05-604e-453b-906a-efca70d47bea.png","subject":"조각투자","name":"불로부터(Issu du feu)","price":"","logo":"https://msr-assets.s3.ap-northeast-2.amazonaws.com/tessaLogo.png","subTitle":"이배","loan":{},"open":"3월 25일 오픈","isbuyNow":"false"},
+        {"img":"https://mkpcdn.com/500x/6c4f6af9af78a210574cb4768434901e_190866.jpg","subject":"NFT","name":"Skull Melt","price":"183,835.5원","logo":"https://msr-assets.s3.ap-northeast-2.amazonaws.com/MPLogo.png","subTitle":"Erik Matson","loan":{},"open":"","isbuyNow":"true"},
+        {"img":"https://treasurer-source.s3.ap-northeast-2.amazonaws.com/trade-item-image/RLX0039/1.png","subject":"조각투자","name":"Yacht-Master II 116680","price":"","logo":"https://treasurer-source.s3.ap-northeast-2.amazonaws.com/trade-item-image/RLX0040/1.png","subTitle":"Rolex","loan":{},"open":"3월 23일 오픈"},
+
+
+    ]});
+
+@api_view(['GET'])
+def focusNFT(request):
+    return Response({"lists":[
+
+        {"img":"https://mkpcdn.com/500x/2b6cdc887fc789845529d867dc0509ec_251793.jpg","subject":"NFT","name":"Deer Pogo","price":"1,950,396.706원","logo":"https://treasurer-source.s3.ap-northeast-2.amazonaws.com/trade-item-image/RLX0040/1.png","subTitle":"giulio iurissevich","loan":{},"open":"","isBuyNow":"true"},
+        {"img":"https://mkpcdn.com/500x/8a8b77c7bdb9df05f53ac5f86b5a8958_978475.jpg","subject":"NFT","name":"Captive Thoughts","price":"","logo":"https://treasurer-source.s3.ap-northeast-2.amazonaws.com/trade-item-image/RLX0040/1.png","subTitle":"ABDULLAH SOYLU","loan":{},"open":"","isBuyNow":""},
+        {"img":"https://mkpcdn.com/500x/a14749e4c5f24b3b6d552653d30ee144_165637.jpg","subject":"NFT","name":"Petals","price":"","logo":"https://treasurer-source.s3.ap-northeast-2.amazonaws.com/trade-item-image/RLX0040/1.png","subTitle":"alexa.art","loan":{},"open":"","isBuyNow":""},
 
 
     ]});
