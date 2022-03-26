3934baf50511cd496d06861d2c85270a97739048
Seoin <tjdls111@naver.com>
2022-03-26 11:46:57 +0900

    feat : 관심 투자 상품 나열

--- front/with-typescript-app/components/interest.tsx
+++ front/with-typescript-app/components/interest.tsx
@@ -10,22 +10,41 @@ const BorderText = styled.span`
   font-weight: bold;
 `;
 
+const Ul = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const ListItem = styled.li`
+  list-style: none;
+  font-size: smaller;
+`;
 function Interest() {
-  const [data, setData] = useState(null);
+  const [datas, setDatas] = useState(null);
 
   useEffect(() => {
     apiGetInvestItems()
       .then((res) => {
-        console.log(res);
+        setDatas(res.data.lists);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+  console.log(datas);
+  console.log(typeof datas);
 
   return (
     <Container>
       <span>관심 가는 투자상품을</span> <BorderText>모두 골라보세요</BorderText>
+      <Ul>
+        <ListItem>{datas[0]}</ListItem>
+        <ListItem>{datas[1]}</ListItem>
+        <ListItem>{datas[2]}</ListItem>
+        <ListItem>{datas[3]}</ListItem>
+        <ListItem>{datas[4]}</ListItem>
+        <ListItem>{datas[5]}</ListItem>
+      </Ul>
     </Container>
   );
 }
